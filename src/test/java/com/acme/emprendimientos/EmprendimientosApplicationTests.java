6dcc34365d438d9eeb6b894fa7f41ddcee749acf
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-16 12:59:06 -0300

    inital commit

---
+++ src/test/java/com/acme/emprendimientos/EmprendimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.emprendimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
